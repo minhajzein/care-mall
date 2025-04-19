a0b0b4c9aa6b09b2cc819c0965dcdb6e9386abab
minhajgrohance <minhaj.grohance@gmail.com>
2025-04-19 15:59:23 +0530

    footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-
 function Footer() {
 	return (
-		<footer className='bg-primary-black text-gray-100 p-2 font-nunito'>
-			<ul>
-				<li></li>
-			</ul>
+		<footer className='bg-primary-black p-8 text-gray-100 font-nunito gap-4 flex flex-col'>
+			<div className='grid grid-cols-2 gap-2'>
+				<ul className='text-xs'>
+					<li className='font-bold'>care mall</li>
+					<li>About US</li>
+					<li>Become a Care Mall Rider</li>
+					<li>in News</li>
+					<li>Privacy Policy</li>
+					<li>Terms & Conditions</li>
+					<li>Careers at Care Mall</li>
+					<li>CM Instant</li>
+					<li>CM Daily</li>
+					<li>CM Blog</li>
+					<li>CM Now</li>
+				</ul>
+				<ul className='text-xs'>
+					<li className='font-bold'>Help</li>
+					<li>FAQs</li>
+					<li>Contact Us</li>
+					<li>CM Wallet FAQs</li>
+					<li>CM Wallet T&Cs</li>
+					<li>Vendor Connect</li>
+				</ul>
+				<ul className='text-xs font-bold'>
+					<li className='capitalize'>cities we serve</li>
+					<li>Our Brands</li>
+					<li>Popular Categories</li>
+				</ul>
+			</div>
+			<p className='text-gray-500 text-[14px]'>Copyright 2025</p>
 		</footer>
 	)
 }
