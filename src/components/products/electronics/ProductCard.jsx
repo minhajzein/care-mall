ab9c32bf65f3eefde47c8567e3d7f7dc21b89b13
minhajgrohance <minhaj.grohance@gmail.com>
2025-04-19 16:27:56 +0530

    wish icon issue solved

--- src/components/products/electronics/ProductCard.jsx
+++ src/components/products/electronics/ProductCard.jsx
@@ -1,13 +1,11 @@
 function ProductCard({ product }) {
 	return (
-		<div className='w-full bg-white p-2 rounded font-nunito gap-[2px] flex flex-col'>
+		<div className='w-full bg-white p-3 rounded font-nunito gap-[2px] flex flex-col'>
 			<div className='w-full relative'>
 				<img className='border border-black' src={product.imageSource} alt='' />
-				<img
-					className='absolute p-[7px] size-6 -top-1 -right-1  rounded-full bg-gray-300'
-					src='/svgs/favourite.svg'
-					alt=''
-				/>
+				<div className='absolute p-[7px] bg-gray-300 -top-1 -right-1 rounded-full'>
+					<img className='size-3' src='/svgs/favourite.svg' alt='' />
+				</div>
 			</div>
 			<h1 className='text-sm line-clamp-2'>{product.title}</h1>
 			<h1 className='font-bold text-lg'>{product.discountPrice}</h1>
