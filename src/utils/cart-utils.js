f4a72bff1cf5c402123356a46edec705693df412
minhajgrohance <minhaj.grohance@gmail.com>
2025-06-05 18:36:09 +0530

    cart retrieved in cart page

--- src/utils/cart-utils.js
+++ src/utils/cart-utils.js
@@ -17,7 +17,7 @@ export const addProductToCart = (product) => {
         return cart;
     }
     let updatedCart = [...cart, { ...product, quantity: 1 }];
-    localStorage.setItem('cart', JSON.stringify(updatedCart));
+    isBrowser && localStorage.setItem('cart', JSON.stringify(updatedCart));
     return updatedCart;
 }
 
