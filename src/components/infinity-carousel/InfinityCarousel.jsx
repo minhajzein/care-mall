33919bab5fd6297e2b4b85744e6c4bd2e29111e0
minhajgrohance <minhaj.grohance@gmail.com>
2025-06-09 16:49:44 +0530

    bts logo changed

--- src/components/infinity-carousel/InfinityCarousel.jsx
+++ src/components/infinity-carousel/InfinityCarousel.jsx
@@ -6,104 +6,104 @@ function InfinityCarousel() {
 			<div className='w-auto grid grid-flow-col'>
 				<div className='w-screen hidden md:grid grid-cols-10 gap-2 p-1 animate-infinite-carousel'>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/10.png'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/11.png'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
-						src='/images/12.png'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
+						src='/images/bts.jpg'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/13.png'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/14.png'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/10.png'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/11.png'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
-						src='/images/12.png'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
+						src='/images/bts.jpg'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/13.png'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/14.png'
 						alt=''
 					/>
 				</div>
 				<div className='w-screen hidden md:grid grid-cols-10 gap-2 p-1 animate-infinite-carousel'>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/10.png'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/11.png'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/bts.jpg'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/13.png'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/14.png'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/10.png'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/11.png'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
-						src='/images/12.png'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
+						src='/images/bts.jpg'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/13.png'
 						alt=''
 					/>
 					<img
-						className='rounded object-contain shadow-md shadow-black/50'
+						className='rounded object-contain shadow-md shadow-black/50 h-12 w-full bg-white'
 						src='/images/14.png'
 						alt=''
 					/>
