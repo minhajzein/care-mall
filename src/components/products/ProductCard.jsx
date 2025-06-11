47450c31bdba3b10c6c6e2f32a594b942dd2f57b
minhajgrohance <minhaj.grohance@gmail.com>
2025-06-11 10:21:56 +0530

    product card re-arranged

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 function ProductCard({ product }) {
 	return (
-		<div className='md:w-full bg-white p-3 rounded w-[160px] shadow-black/50 shadow-md font-nunito gap-[2px] flex flex-col'>
+		<div className='md:w-full bg-white p-3 rounded w-[170px] shadow-black/50 shadow-md font-nunito gap-[2px] flex flex-col'>
 			<div className='w-full relative'>
 				<Link className='w-full' href={`/products/${product.title}`}>
 					<img
@@ -19,14 +19,14 @@ function ProductCard({ product }) {
 			</div>
 			<h1 className='text-sm truncate'>{product.title}</h1>
 			<Rate value={4.5} style={{ fontSize: '12px' }} allowHalf disabled />
-			<h1 className='font-bold text-lg'>₹{product.discountPrice}</h1>
+			<h1 className='font-bold text-sm'>₹{product.discountPrice}</h1>
 			<div className='flex gap-2 items-center'>
 				<del className='text-gray-500 text-xs'>₹{product.price}</del>
 				<div className='bg-green-500/20 rounded text-green-500 text-[10px] p-1'>
 					15% OFF
 				</div>
 			</div>
-			<button className='uppercase border border-black rounded-lg mt-1 text-red-500 font-bold'>
+			<button className='uppercase border text-xs border-primary rounded-lg mt-1 p-1 text-nowrap text-red-500 font-bold'>
 				add to cart
 			</button>
 		</div>
